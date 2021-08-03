4f557715791bbe35972cce10cb18872c751b1ae8
nekoimi <nekoimime@gmail.com>
2021-08-03 11:11:32 +0800

    up

--- src/main/java/com/nekoimi/boot/framework/configuration/properties/RoutePatternsProperties.java
+++ src/main/java/com/nekoimi/boot/framework/configuration/properties/RoutePatternsProperties.java
@@ -5,9 +5,6 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * nekoimi  2021/7/20 下午5:06
  */
@@ -16,11 +13,4 @@ import java.util.List;
 @Component
 @ConfigurationProperties(prefix = "app.route")
 public class RoutePatternsProperties {
-    private PathPattern authorization = new PathPattern();
-
-    @Getter
-    @Setter
-    public static final class PathPattern {
-        private List<String> exclude = new ArrayList<>();
-    }
 }
